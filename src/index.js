dc363b6756ac5cb75465490af8b6fd8c4bf354bc
Brendan Humphries <brendan.b.humphries@gmail.com>
2021-09-28 16:59:45 -0400

    commit tree 9627c2eebe5a1239918749c775243e73aed250e0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(images => images.message.forEach(renderImages))
+    
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(breeds => {
+        for (const item in breeds.message) {
+            addBreeds(item);
+        }
+    })
+
+    const breedListItem = document.querySelector('#dog-breeds');
+    breedListItem.addEventListener('click', colorAClickedLi);
+})
+
+function renderImages(image) {
+    const newItem = document.createElement('img');
+    const imageContainer = document.querySelector('#dog-image-container');
+    newItem.src = image;
+    imageContainer.append(newItem);
+}
+
+function addBreeds(breed) {
+    const newItem = document.createElement('li');
+    const breedsContainer = document.querySelector('#dog-breeds');
+    newItem.textContent = breed;
+    breedsContainer.append(newItem);
+}
+
+function colorAClickedLi(event) {
+    event.target.style.color = 'red';
+}
